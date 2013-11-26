906f24fdc8e58ab976e114d153f8343cabafe63c
Ghanil Yusof <ghanil.yusof@gmail.com>
2013-11-26 15:31:29 +0800

    Get User data from configuration

--- src/main/java/jomsocial/users/Users.java
+++ src/main/java/jomsocial/users/Users.java
@@ -1,11 +1,13 @@
 package jomsocial.users;
 
+import jomsocial.config.Config;
+
 public class Users {
 
     // todo - from configuration
     // todo - make unique for testing session
-    public static final User USER_A = Users.user("user-a", "password");
-    public static final User USER_B = Users.user("user-b", "password");
+    public static final User USER_A = Users.user(Config.get("USER_A"), Config.get("USER_A_PASSWORD"));
+    public static final User USER_B = Users.user(Config.get("USER_B"), Config.get("USER_B_PASSWORD"));
 
     /**
      * If a user has been created with the given name it will be returned. If the user has not yet been created it will
